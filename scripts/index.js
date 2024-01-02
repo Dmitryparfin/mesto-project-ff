508b4b8b1a64548f7a96ba12c3a8f7d5b59e5da0
Dima Parfinovich <dmitry.parfinovitch@yandex.ru>
2024-01-02 21:48:27 +0300

    добавлены функции создания и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,26 @@
 const container = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-function addCard({name, link}) {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    const deleteButton = cardElement.querySelector('.card__delete-button');
+function createCard({name, link}) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-    cardElement.querySelector('.card__image').src = link;
-    cardElement.querySelector('.card__image').alt = name;
-    cardElement.querySelector('.card__title').textContent = name;
-    deleteButton.addEventListener('click', function() {
-        cardElement.remove();
-      }
-    );
-    container.prepend(cardElement);
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__image').alt = name;
+  cardElement.querySelector('.card__title').textContent = name;
+
+  deleteButton.addEventListener('click', () =>
+    deleteCard(cardElement)
+  );
+
+  return cardElement;
 }
 
-initialCards.forEach(addCard);
+function deleteCard(cardElement) {
+  cardElement.remove(); 
+}
+
+initialCards.forEach(function (cardElement) {
+  const card = createCard(cardElement);
+  container.prepend(card);
+});
